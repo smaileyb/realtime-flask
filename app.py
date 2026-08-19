22148425a437d1051dfed97d3862d47ffe342ccf
Smailey Alexandre Behm <smailey.behm@gmail.com>
2026-08-19 09:23:02 -0300

    feat(database): initialize SQLAlchemy and create Payment model

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, jsonify
+from repository.database import db
+from db_models.payment import Payment
 
+# Create the Flask application instance.
+# This object represents the web app and is used to register routes and configure settings.
 app = Flask(__name__)
 
+# Configure the database connection for the app.
+# SQLite database file stored in the project directory as 'payments.db'.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///payments.db'
+
+# Set a secret key used by Flask for session management and security-related features.
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+# Initialize SQLAlchemy with this Flask app so database models can be used.
+# This binds the database extension to the application instance.
+db.init_app(app)
+
+#Routes
 # Define payment route using PIX
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
---
+++ db_models/payment.py
@@ -0,0 +1,10 @@
+from repository.database import db
+
+class Payment(db.Model):
+  # id, value, paid, bank_payment_id, qr_code, expiration_date
+  id = db.Column(db.Integer, primary_key=True)
+  value = db.Column(db.Float, nullable=False)
+  paid = db.Column(db.Boolean, default=False)
+  bank_payment_id = db.Column(db.Integer, nullable=True)
+  qr_code = db.Column(db.String(255), nullable=True)
+  expiration_date = db.Column(db.DateTime, nullable=True)
