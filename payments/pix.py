c3dece45cabac74663795e9b9a05af6a5d70a2e6
Smailey Alexandre Behm <smailey.behm@gmail.com>
2026-08-19 09:53:02 -0300

    feat(payment): implement Pix class for payment creation and QR code generation

---
+++ payments/pix.py
@@ -0,0 +1,20 @@
+import uuid
+import qrcode
+
+class Pix:
+  def __init__(self):
+    pass
+  def create_payment(self, value):
+    # create the payment in the financial institution
+    bank_payment_id = uuid.uuid4()
+
+    #simulate code for qr code generation
+    hash_payment = f'hash_payment_{bank_payment_id}'
+
+    # Generate QR code and save it as an image file
+    qr_code_img = qrcode.make(hash_payment)
+    qr_code_img.save(f'static/img/qr_code_payment_{bank_payment_id}.png')
+
+    
+    return {"bank_payment_id": bank_payment_id, 
+            "qr_code_path": f'qr_code_payment_{bank_payment_id}'}
