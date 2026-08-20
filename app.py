2b630ef82b61b77d4a7d82d0099b17585e4167cf
Smailey Alexandre Behm <smailey.behm@gmail.com>
2026-08-20 10:21:06 -0300

    feat: Implement payment confirmation and QR code display
    - Updated app.py to render payment details on the payment page.
    - Modified request.http to change the payment value for testing.
    - Added styles.css for consistent styling across payment pages.
    - Created SVG icons for UI elements including check, clock, currency, and basket.
    - Developed 404.html for handling not found errors with user-friendly messaging.
    - Created confirmed_payment.html to display successful payment confirmation.
    - Developed payment.html to show payment details and QR code for user interaction.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -51,7 +51,13 @@ def confirm_payment_pix():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-  return 'Pix payment'
+  payment = Payment.query.get(payment_id)
+
+  return render_template('payment.html', 
+                         payment_id=payment.id, 
+                         payment_value=payment.value, 
+                         host='http://localhost:5000', 
+                         qr_code=payment.qr_code)
 
 if __name__ == '__main__':
   app.run(debug=True)
