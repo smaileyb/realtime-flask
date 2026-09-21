192c0912f6394357384b09fabe5246440c458cd4
Smailey Alexandre Behm <smailey.behm@gmail.com>
2026-09-21 09:20:14 -0300

    feat(payment): update create_payment method to accept base directory for QR code storage

--- payments/pix.py
+++ payments/pix.py
@@ -4,7 +4,7 @@ import qrcode
 class Pix:
   def __init__(self):
     pass
-  def create_payment(self, value):
+  def create_payment(self, base_dir=""):
     # create the payment in the financial institution
     bank_payment_id = str(uuid.uuid4())
 
@@ -13,7 +13,7 @@ class Pix:
 
     # Generate QR code and save it as an image file
     qr_code_img = qrcode.make(hash_payment)
-    qr_code_img.save(f'static/img/qr_code_payment_{bank_payment_id}.png')
+    qr_code_img.save(f'{base_dir}static/img/qr_code_payment_{bank_payment_id}.png')
 
     
     return {"bank_payment_id": bank_payment_id, 
